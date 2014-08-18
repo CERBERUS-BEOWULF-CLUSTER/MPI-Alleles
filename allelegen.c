1c84907e7ec14d06fac71fe6fd06c9a24702c212
Tony Sena <gsena@massbay.edu>
2014-08-18 15:18:17 -0400

    new version of allelegen.c
    Richard Hayes:
    "Here is a more advanced allelegen.c. It shouldn't change the
    implementation of anything if you replace my old code with this,
    but keep a backup just in case. Let me know if you have any
    questions.
    
    My phone: 617-999-4000" (11/11/13)

--- allelegen.c
+++ allelegen.c
@@ -2,8 +2,10 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
 int atoi(const char *nptr);
+int probability(void);
 
 int main(int argc, char* argv[]) {
     //int numsamples = atoi(argv[1]);
@@ -11,6 +13,8 @@ int main(int argc, char* argv[]) {
     char* filename = "samples.txt";
     char buffer[4];
     int i;
+    const char* loci[15] = {"D8S1179", "D21S11", "D7S820", "CSF1PO", "D3S1358", "TH01", "D13S317", 				    "D16S539", "D2S1338", "D19S433", "vWA", "TPOX", "D18S5I", "D5S818", "FGA"};
+    const char* symbols[5] = {">", "<", "\"", "INC", "NS"};
 
     FILE *file = fopen(filename, "w");
     srand(time(0));
@@ -23,19 +27,42 @@ int main(int argc, char* argv[]) {
         fwrite("\n", 1, sizeof(char), file);
         int j;
         for(j = 0; j < 15; j++) {
-            fwrite("locus", 1, strlen("locus"), file);
-            sprintf(buffer, "%d", j);
-            fwrite(buffer, 1, strlen(buffer), file);
+            fwrite(loci[j], 1, strlen(loci[j]), file);
             fwrite("[", 1, sizeof(char), file);
             fwrite("\n", 1, sizeof(char), file);
             int k;
-            int l = rand()%7+1;
-            for(k = 0; k < l; k++) {
-                int a = rand()%30+1;
-                sprintf(buffer, "%d", a);
+            int l = probability();
+	    if(l > 0) {
+            	for(k = 0; k < l; k++) {
+                    int a = rand()%30+1;
+                    sprintf(buffer, "%d", a);
+                    fwrite(buffer, 1, strlen(buffer), file);
+		    if(rand()%100+1 >= 92) {
+			if(rand()%2+1 == 1) {
+			    fwrite("<", 1, sizeof(char), file);
+			}
+			else {
+                    	    fwrite(">", 1, sizeof(char), file);
+			}
+		    }
+                    fwrite("\n", 1, sizeof(char), file);
+            	}
+	    }
+	    else if(l == 0) {
+		sprintf(buffer, "%s", symbols[4]);
                 fwrite(buffer, 1, strlen(buffer), file);
                 fwrite("\n", 1, sizeof(char), file);
-            }
+	    }
+	    else {
+		sprintf(buffer, "%s", symbols[2]);
+                fwrite(buffer, 1, strlen(buffer), file);
+		int a = rand()%30+1;
+                sprintf(buffer, "%d", a);
+                fwrite(buffer, 1, strlen(buffer), file);
+		sprintf(buffer, "%s", symbols[2]);
+                fwrite(buffer, 1, strlen(buffer), file);
+                fwrite("\n", 1, sizeof(char), file);
+	    }
             fwrite("]", 1, sizeof(char), file);
             fwrite("\n", 1, sizeof(char), file);
         }
@@ -43,4 +70,23 @@ int main(int argc, char* argv[]) {
         fwrite("\n", 1, sizeof(char), file);
     }
     fclose(file);
+}
+
+int probability(void) {
+    int r = rand()%100+1;
+    if(r <= 85) {
+	return 2; //has two alleles
+    }
+    else if(r <= 90) {
+	return 1; //has only one allele
+    }
+    else if(r <= 97) {
+	return rand()%4+3; //has three or more alleles
+    }
+    else if(r <= 98){
+	return -1; //Inconclusive
+    }
+    else {
+	return 0; //None seen
+    }
 }
